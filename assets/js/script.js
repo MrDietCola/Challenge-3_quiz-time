91564719eb9d5cc02a6a458249664b01cd4371ec
AJ <ajseidler0526@gmail.com>
2023-09-22 00:15:24 -0500

    added starting quiz text and have the quiz begin with a button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,24 @@ function timerStart() {
     msEl.textContent = msLeft;
     secondsEl.textContent = secondsLeft;
 
-
     if (msLeft === 0) {
       secondsLeft -= 1;
       msLeft = 10;
     } else if (secondsLeft === 0) {
-      clearInterval(timerInterval)
+      msLeft = 0;
+      msEl.textContent = msLeft;
+      clearInterval(timerInterval);
     } 
-
   }, 100);
 }
 
-timerStart();
-
 function reduceTime() {
   secondsLeft -= 15;
-  console.log("clicked")
+}
+
+function startQuiz () {
+  document.querySelector("#start-quiz").style.display = "none";
+  document.querySelector("#question-container").style.display = "block";
+  timerStart()
+
 }
