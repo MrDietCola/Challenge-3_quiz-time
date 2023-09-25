b44445a8cd50335712721ee238e84ec15f3c4b82
AJ <ajseidler0526@gmail.com>
2023-09-25 10:05:34 -0500

    added comments throughout css

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,10 +1,31 @@
 var storedScores = JSON.parse(localStorage.getItem("scored")) || [];
-console.log(storedScores);
-// storedScores = JSON.parse(storedScores);
-
-document.querySelector("#name").innerHTML = storedScores[0].initials;
-document.querySelector("#score").innerHTML = storedScores[0].playerScore;
 
 function resetScores() {
   localStorage.setItem("scored", "[]");
+  window.location = "leaderboard.html";
 }
+
+function tryAgain() {
+  window.location = "index.html";
+}
+
+
+// create score element
+function createScoreboard() {
+  for (i=0; i < storedScores.length; i++) {
+    var scoreLi = document.createElement("li");
+    var initialsLi = document.createElement("li");
+    initialsLi.textContent = storedScores[i].initials;
+    scoreLi.textContent = storedScores[i].playerScore;
+    document.querySelector("#initials").appendChild(initialsLi);
+    document.querySelector("#scores").appendChild(scoreLi);
+  }
+}
+
+function reloadScoreboard() {
+document.querySelector("#initials").textContent = "";
+document.querySelector("#scores").textContent = "";
+createScoreboard();
+}
+
+reloadScoreboard();
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,32 +4,33 @@ var secondsLeft = 29;
 var msLeft = 10;
 var index = 0;
 var score = 0;
-localStorage.getItem("score");
 var endQuiz = false;
+
+// this is an array of all the possible questions
 var questions = [
   {
-    question: "question 1",
-    choices: ["text 1", "text 2", "text 3", "text 4"],
-    answer: "text 1"
+    question: "In what country did the first Starbucks open outside of North America",
+    choices: ["Mexico", "Japan", "Australia", "France"],
+    answer: "Japan"
   },
   {
-    question: "question 2",
-    choices: ["text 211", "text 212", "text 213", "text 214"],
-    answer: "text 212"
+    question: "What is the tiny piece at the end of a shoelace called?",
+    choices: ["Aglet", "Turne", "Bean", "Stitch"],
+    answer: "Aglet"
   },
   {
-    question: "question 3",
-    choices: ["text 31", "text 32", "text 33", "text 34"],
+    question: "What year did Facebook first launch?",
+    choices: ["2008", "2006", "2004", "2002"],
     answer: "text 32"
   },
   {
-    question: "question 4",
-    choices: ["text 41", "text 442", "text 43", "text 4"],
-    answer: "text 442"
+    question: "What was the coffee shop called in the hit TV show Friends",
+    choices: ["Central Park", "Park Central", "Central Perk", "Downtown Coffee"],
+    answer: "Central Perk"
   }
  ]
 
-// create header for question
+// create header for question prompt
 var questionPrompt = document.createElement("h2");
 // create ordered list element
 var answerChoices = document.createElement("ol");
@@ -41,10 +42,11 @@ var choice4 = document.createElement("li");
 // create correct/incorrect element
 var answerResultP = document.createElement("p");
 
+// event listener on each answer choice and then comparing to correct answer and adding to score or subtracting from time remaining
 answerChoices.addEventListener("click", function(event) {
   // event.stopPropagation();
   var answerIndex = index;
-    var answerChosen = event.target;
+  var answerChosen = event.target;
   if (answerChosen.textContent === questions[answerIndex].answer) {
     score+=5;
     answerResultP.textContent = "Correct";
@@ -52,7 +54,7 @@ answerChoices.addEventListener("click", function(event) {
     document.querySelector("#question-container").appendChild(answerResultP);
   } else { 
     secondsLeft -= 5;
-    answerResultP.textContent = "Inorrect"
+    answerResultP.textContent = "Incorrect"
     answerResultP.style.order = "3";
     document.querySelector("#question-container").appendChild(answerResultP);
   } 
@@ -72,26 +74,31 @@ function timerStart() {
     msLeft--;
     msEl.textContent = msLeft;
     secondsEl.textContent = secondsLeft;
-
+// check if end quiz was set to true
     if (endQuiz) {
       clearInterval(timerInterval);
+// resetting ms if they reach 0
     } else if (msLeft === 0) {
       secondsLeft -= 1;
       msLeft = 10;
+// if seconds is at zero, end quiz and show the score
     } else if (secondsLeft === 0) {
       msLeft = 0;
       msEl.textContent = msLeft;
       clearInterval(timerInterval);
-    } 
+      showScore()
+    }
   }, 100);
 }
 
+// quiz start function to show the first question and start the timer function
 function startQuiz () {
   document.querySelector("#start-quiz").style.display = "none";
   questionCycle()
   timerStart()
 }
 
+// logic for cycling through questions and using the index variable to choose the question from array (index is increased after they choose an answer)
 function questionCycle() {
   questionPrompt.textContent = questions[index].question;
   choice1.textContent = questions[index].choices[0];
@@ -109,6 +116,7 @@ function questionCycle() {
   answerChoices.appendChild(choice4);    
 }
 
+// showing the players score and setting endQuiz to true to stop timer and hiding question text
 function showScore() {
   endQuiz = true;
   document.querySelector("#correct-bonus").innerHTML = "+" + score;
@@ -118,34 +126,33 @@ function showScore() {
   document.querySelector("#question-container").style.display = "none";
 }
 
+// event listener for the submit button to submit score and initials
 document.querySelector("#submit").addEventListener("click", function () {
-  window.location = "leaderboard.html";
-  storeScore();
+// checking if the initials inputted fit the criteria 
+  var initials = document.querySelector("#initials").value.split("");
+  if (initials.length > 3 || initials.length === 0 || initials.includes(" ")) {
+    document.querySelector("#alert").innerHTML = "Initials must be 1-3 letters and not include spaces";
+// changing page to leaderboard page and running store score function and clearing initials box text
+  } else {
+    window.location = "leaderboard.html";
+    storeScore();
+    document.querySelector("#initials").value = "";
+  }
 })
 
-// localStorage.setItem("scored", "[]");
-
-// console.log(storedScores);
-
+// creating object of user score, getting the scores from local storage, then adding the new user score to the stored scores
 function storeScore() {
   var scoreToStore = {
     initials: document.querySelector("#initials").value,
     playerScore: score,
   };
   var storedScores = JSON.parse(localStorage.getItem("scored")) || [];
-  // storedScores.push(JSON.stringify(scoreToStore));
-  storedScores.push((scoreToStore));
-  console.log((storedScores))
+  storedScores.unshift((scoreToStore));
   localStorage.setItem("scored", (JSON.stringify(storedScores)));
 }
 
-function restart() {
-  index = 0;
-  score = 0;
-  secondsLeft = 29;
-  msLeft = 10;
-  document.querySelector("#question-container").style.display = "flex";
-  questionCycle()
-  timerStart()
-}
+// taking user to leaderboard page
+  document.querySelector("#high-scores").addEventListener("click", function () {
+  window.location = "leaderboard.html";
+});
 
